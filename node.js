767036fd52417b01596d1b1b1980cffcb5ecd80c
Saksham <sakshamsatnalika34@gmail.com>
2024-02-08 13:13:11 +0530

    modified:   node.js 	.vscode/ 	a.html 	b.html 	d.html

--- node.js
+++ node.js
@@ -9,15 +9,16 @@
 //     console.log('Interval stopped');
 // },5000);
 
-function parentFunction(name,callback)
-{
-    //setTimeout(callback(),1000);
-    callback();
-    console.log("Hey"+name);
-}
-function randomFunction()
-{
-    console.log("Hey I am callback function");
-}
+// function parentFunction(name,callback)
+// {
+//     //setTimeout(callback(),1000);
+//     callback();
+//     console.log("Hey"+name);
+// }
+// function randomFunction()
+// {
+//     console.log("Hey I am callback function");
+// }
 
-parentFunction("Random String",randomFunction);
+// parentFunction("Random String",randomFunction);
+
